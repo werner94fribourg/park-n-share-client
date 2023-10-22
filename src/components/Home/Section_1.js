7ef1dbeeccb2b63bc72d0657fccb9e0b3aac6b27
ahonongobi <abyssiniea@gmail.com>
2023-10-22 17:53:34 +0200

    add sign in pages with design

--- src/components/Home/Section_1.js
+++ src/components/Home/Section_1.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import { Link } from 'react-router-dom';
 
 const ButtonGroup = () => {
   const buttonStyle = {
-    fontSize: '24px', // Augmente la taille du texte
-    borderRadius: '50px', // Arrondit les boutons
+    fontSize: '24px',
+    borderRadius: '50px',
+    '&:hover': {
+      boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.2)',
+    },
   };
 
   return (
     <Box display="flex" justifyContent="center" mt={2} mb={3}>
-      <Button variant="contained" color="primary" size="large" sx={buttonStyle} style={{ margin: '0 10px' }}>
+       <Button variant="contained" color="primary" size="large" sx={buttonStyle} style={{ margin: '0 10px' }}>
         Free Parking
       </Button>
-      <Button variant="contained" color="primary" size="large" sx={buttonStyle} style={{ margin: '0 10px' }}>
+      
+      <Button component={Link} to="/signup" variant="contained" color="primary" size="large" sx={buttonStyle} style={{ margin: '0 10px' }}>
         Sign Up
       </Button>
-      <Button variant="contained" color="primary" size="large" sx={buttonStyle} style={{ margin: '0 10px' }}>
+      <Button component={Link} to="/signin" variant="contained" color="primary" size="large" sx={buttonStyle} style={{ margin: '0 10px' }}>
         Sign In
       </Button>
     </Box>
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,14 @@
 import Home from '../pages/Home';
 import { Route, Routes } from 'react-router';
+import SignIn from '../pages/Signin';
+import SignUp from '../pages/Signup';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="*" element={<Home />} />
+      <Route path='/signin' element={<SignIn />} />
+      <Route path='/signup' element={<SignUp />} />
     </Routes>
   );
 };
