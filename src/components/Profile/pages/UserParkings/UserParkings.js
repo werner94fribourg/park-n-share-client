fbc4a17753460caf3d350193baac29c8b6da9324
ahonongobi <abyssiniea@gmail.com>
2023-11-23 16:57:18 +0100

    make table

--- src/components/Profile/pages/UserParkings/UserParkings.js
+++ src/components/Profile/pages/UserParkings/UserParkings.js
@@ -1,7 +1,3 @@
-import { Button } from '@mui/material';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
-import { AgGridReact } from 'ag-grid-react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 
@@ -18,35 +14,43 @@ const UserParkings = () => {
   };
 
   const columns = [
-    { field: 'id', headerName: 'ID' },
-    { field: 'name', headerName: 'Name' },
-    { field: 'description', headerName: 'Description', width: 200 },
-    { field: 'type', headerName: 'Type', width: 50 },
     {
-      field: 'price',
-      headerName: 'CHF/H',
-      type: 'number',
-      width: 50,
-      align: 'center',
-    },
-    { field: 'address', headerName: 'Address', width: 200 },
-    {
-      field: 'validated',
-      headerName: 'Validated',
-      width: 50,
+      key: 'avatar',
+      label: 'ID',
     },
     {
-      field: 'view',
-      headerName: '',
-      hide: true,
-      cellRendererFramework: params => (
-        <Button
-          type="button"
-          onClick={viewParkingHandler.bind(null, params.data.id)}
-        >
-          View
-        </Button>
-      ),
+      key: 'name',
+      label: 'Name',
+    },
+
+    {
+      key: 'description',
+      label: 'Description',
+      _style: { width: 200 },
+    },
+    {
+      key: 'type',
+      label: 'Type',
+      _style: { width: 50 },
+    },
+    {
+      key: 'price',
+      label: 'CHF/H',
+      _style: { width: 50, textAlign: 'center' },
+    },
+    {
+      key: 'address',
+      label: 'Address',
+      _style: { width: 200 },
+    },
+    {
+      key: 'validated',
+      label: 'Validation',
+      _style: { width: 50 },
+    },
+    {
+      key: 'view',
+      label: '',
     },
   ];
 
@@ -62,32 +66,18 @@ const UserParkings = () => {
     validated: parking.isValidated ? 'Yes' : 'No',
   }));
 
-  //TODO: get my parkings + update state
-  return (
-    <>
-      {/*<DataGrid
-        columns={columns}
-        rows={processedParkings}
-        sx={{ position: 'absolute', width: '100%' }}
-        initialState={{
-          pagination: { paginationModel: { page: 0, pageSize: 5 } },
-        }}
-        pageSizeOptions={[5, 10]}
-      />*/}
-      <div style={{ height: 400, width: '100%' }}>
-        <AgGridReact
-          columnDefs={columns}
-          rowData={processedParkings}
-          domLayout="autoHeight"
-          pagination={true}
-          paginationPageSize={5}
-        />
-      </div>
-      <Button type="button" onClick={newParkingHandler}>
-        New Parking Request
-      </Button>
-    </>
-  );
+  const getBadge = validated => {
+    switch (validated) {
+      case 'Yes':
+        return 'success';
+      case 'No':
+        return 'danger';
+      default:
+        return 'danger';
+    }
+  };
+
+  return <div></div>;
 };
 
 export default UserParkings;
